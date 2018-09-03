065ceb09494f8a665e70298f3aa4946a5da2c2e3
admin <admin@git.com>
2018-09-03 10:37:24 +0800

    update

---
+++ server/wxconfig.php
@@ -0,0 +1,15 @@
+<?php
+
+$DBConfig = array(
+  'DB_USER'   => 'root',                  // 用户名
+  'DB_PWD'    => '123456',                // 数据库用户密码
+);
+
+$WxConfig = array(
+  'APPID'     => 'wx5afa288e60d81f0e',	  // 微信appid
+  'SECRET'    => '',                      // 微信secret
+
+  'MCHID'     => '',
+  'KEY'       => '',
+  'NOTIFY_URL'=> 'https://scwhbs.com/index.php/Api/Wxpay/notify',
+);
