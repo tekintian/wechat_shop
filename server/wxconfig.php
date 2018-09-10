6dcd6053c229b7335050390fcc7bf0d292e62b92
admin <admin@git.com>
2018-09-10 08:58:27 +0800

    update

--- server/wxconfig.php
+++ server/wxconfig.php
@@ -3,12 +3,13 @@
 class WxConfig {
   const APPID           = 'wx5afa288e60d81f0e';               // 微信appid
   const SECRET          = '34d14ff3408722e42954aae293ef40a9'; // 微信secret
-
-  const MCHID           = '1511828741';
-  const KEY             = 'sxylhaobangshoukejisongcaiwa2018';
   const NOTIFY_URL      = 'https://scwhbs.com/index.php/Api/Wxpay/notify';
 
-  // const WXPAY_HTTPS  = 'https://api.mch.weixin.qq.com';
+  const MCHID           = '1511828741';
+  // const KEY             = 'sxylhaobangshoukejisongcaiwa2018';
+  // const WXPAY_HTTPS     = 'https://api.mch.weixin.qq.com';
+
+  const KEY             = 'sxylhaobangshoukejisongcaiwa2018';
   const WXPAY_HTTPS     = 'https://api.mch.weixin.qq.com/sandboxnew';
 }
 
